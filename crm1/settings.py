6ebf0e0c2cf4b0e73cdc065621a126ed115db066
miseriae <zuyev.frm@gmail.com>
2020-05-12 18:38:38 +0300

    Add basic models

--- crm1/settings.py
+++ crm1/settings.py
@@ -41,6 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'accounts',
+
+    'phonenumber_field',
 ]
 
 MIDDLEWARE = [
@@ -122,3 +124,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_URL = '/images/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
