95e98c9310cb6ca60781ed5cb3bc44c48b23d0fe
miseriae <zuyev.frm@gmail.com>
2020-05-12 18:34:51 +0300

    Add basic view

---
+++ accounts/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+
+app_name = 'accounts'
+
+urlpatterns = [
+    path('', views.home),
+    path('products/', views.products),
+    path('customer/', views.customer),
+]
