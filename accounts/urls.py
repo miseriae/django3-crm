589c091884f67fcb788d7abb3e0c88e7eec02762
miseriae <zuyev.frm@gmail.com>
2020-05-14 18:30:37 +0300

    Add CRUD functionality

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,7 +5,10 @@ from . import views
 app_name = 'accounts'
 
 urlpatterns = [
-    path('', views.home),
-    path('products/', views.products),
-    path('customer/<str:pk>', views.customer),
+    path('', views.home, name='home'),
+    path('products/', views.products, name='products'),
+    path('customer/<str:pk>/', views.customer, name='customer'),
+    path('create_order/', views.create_order, name='create_order'),
+    path('update_order/<str:pk>/', views.update_order, name='update_order'),
+    path('delete_order/<str:pk>/', views.delete_order, name='delete_order'),
 ]
